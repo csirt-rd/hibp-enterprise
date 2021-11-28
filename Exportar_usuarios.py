a8a35a12b8e5ad46178f45e61a718f2e114ce9d0
Oscar Encarnacion Liz <87453279+oencarnacion@users.noreply.github.com>
2021-11-28 12:13:29 -0400

    Update Exportar_usuarios.py

--- Exportar_usuarios.py
+++ Exportar_usuarios.py
@@ -6,7 +6,7 @@ def request_api(domain):
 
     try: 
 
-        headers = { 'Authorization': 'Bearer C63ogLvJNSN_kP9cv-TyiOrc6-eYN5HfplElWhKsf3qzP-7yg68o38yBbepMueh12iM5d6thTx9dAQEzNHkKxdQkXjIoBFQa5uTmCqJtWxw-usVa4w_71_SZyaABrZUdSS8qAEoMqmO2vyygttwt-jgl9A-zOgjdP_h-Kjw9DJINhuZGnKTRVE1Xjfv0mXoqE8KctwYQD2UXePN8K1QKRRN0OV691Q-Rxe9bvCX4XVv-GV-f3X4O2NZ-Jf6wMs0Fi9nhVNgYdzW0wMTr_zgWyGstPjWiQMsvV8PNLX0iB_guiF6P0EEdQP8Wwl6eOWeOAQ2l6v1KJYxHbi9FargW9iMcMJOLUMN4tHxbdk7SzqJvvYVqoaQ8FFopPMuePPeslOGetDjTSLLR5XFKAjPrbCJTu0JBwPtiZ4ftQ3H8dKI','Content-Type': 'application/x-www-form-urlencoded'}
+        headers = { 'Authorization': 'Bearer <TOKEN>','Content-Type': 'application/x-www-form-urlencoded'}
 
         url = "https://haveibeenpwned.com/api/v2/enterprisesubscriber/domainsearch/{}".format(domain) 
 
