4ebd7c9003199ea322d454fe66cc0d15aee0cc46
Oscar Encarnacion Liz <87453279+oencarnacion@users.noreply.github.com>
2021-11-28 12:12:57 -0400

    Update Importar_dominios.py

--- Importar_dominios.py
+++ Importar_dominios.py
@@ -1,7 +1,3 @@
-############################################################################################################################################################
-#Con este script importamos los dominios x.gob.do que se encuentren en un archivo proporcionado que contiene los dominios validos mencionados anteriolmente.
-############################################################################################################################################################
-
 import requests #Importamos la biblioteca request para poder enviar solicitudes HTTP al servidor Have I Been Pwned.
 
 filename = 'Dominios.txt' #Seleccionamos la ruta del archivo.
@@ -15,7 +11,7 @@ with open(filename) as obj: #Abrimos el archivo para luego leerlo.
 
         url = 'https://haveibeenpwned.com/api/v2/enterprisesubscriber/domain' #URL utilizada para añadir dominios esta info se encuentra en la documentacion.
 
-        headers = { 'Authorization': 'Bearer C63ogLvJNSN_kP9cv-TyiOrc6-eYN5HfplElWhKsf3qzP-7yg68o38yBbepMueh12iM5d6thTx9dAQEzNHkKxdQkXjIoBFQa5uTmCqJtWxw-usVa4w_71_SZyaABrZUdSS8qAEoMqmO2vyygttwt-jgl9A-zOgjdP_h-Kjw9DJINhuZGnKTRVE1Xjfv0mXoqE8KctwYQD2UXePN8K1QKRRN0OV691Q-Rxe9bvCX4XVv-GV-f3X4O2NZ-Jf6wMs0Fi9nhVNgYdzW0wMTr_zgWyGstPjWiQMsvV8PNLX0iB_guiF6P0EEdQP8Wwl6eOWeOAQ2l6v1KJYxHbi9FargW9iMcMJOLUMN4tHxbdk7SzqJvvYVqoaQ8FFopPMuePPeslOGetDjTSLLR5XFKAjPrbCJTu0JBwPtiZ4ftQ3H8dKI','Content-Type': 'application/x-www-form-urlencoded'}
+        headers = { 'Authorization': 'Bearer <TOKEN>','Content-Type': 'application/x-www-form-urlencoded'}
 
         payload = {'domain': i.strip()} #Como key la pasamos el dominio y luego le pasamos el dominio leido por el script.
 
